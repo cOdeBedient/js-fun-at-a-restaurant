9d27050a208c1fc27f3ffe4f85ecf50854838cba
Adam Bedient <adambedient@gmail.com>
2023-11-30 09:19:33 -0500

    Complete first pass

--- src/meal.js
+++ src/meal.js
@@ -1,7 +1,37 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`
 }
 
+function createMenuItem(item, price, type){
+  var menuItem = {
+    name: ${item},
+    price: ${price},
+    type: ${type},
+  }
+  return menuItem
+}
+
+function addIngredients(ingredient, array){
+  return array.push(ingredient);
+}
+
+function formatPrice(price){
+  return `$${price}`;
+}
+
+function decreasePrice(price){
+  var discountPrice = price - price/10;
+  return discountPrice;
+}
+
+function createRecipe(title, ingredients, type){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe;
+}
 
 module.exports = {
   nameMenuItem,
